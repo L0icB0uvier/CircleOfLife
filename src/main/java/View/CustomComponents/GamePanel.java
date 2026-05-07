8f6d91c405a57e57b6a5da0aa8cc757c1098ba24
L0icB0uvier <loic.bouvier@protonmail.com>
2026-05-07 15:44:36 +0200

    Ajout pierres preview et disabled.

--- src/main/java/View/CustomComponents/GamePanel.java
+++ src/main/java/View/CustomComponents/GamePanel.java
@@ -1,11 +1,6 @@
 package View.CustomComponents;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.Stroke;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.InputStream;
 import java.util.List;
@@ -15,15 +10,23 @@ import javax.swing.JComponent;
 
 import Global.Configuration;
 import Model.Coordinate;
+import Model.CoordinateUtils;
 import Model.Game;
 import Model.Match;
 import Patterns.Observer;
-import View.Utils.UIColor;
 
 public class GamePanel extends JComponent implements Observer {
     Game game;
-    BufferedImage imgPlateau, imgStonePlayer1, imgStonePlayer2, imgEatenStone;
     Match match;
+
+    BufferedImage
+            imgPlateau,
+            imgStonePlayer1,
+            imgStonePlayer2,
+            imgStonePlayer1Preview,
+            imgStonePlayer2Preview,
+            imgEatenStone,
+            imgStoneDisabled;
 
     int imgSrcHeight, imgSrcWidth;
     int mouseX, mouseY;
@@ -31,22 +34,15 @@ public class GamePanel extends JComponent implements Observer {
 
     boolean previewEnabled = false;
 
-    double incX;
-    double incY;
-    double size;
-    int stoneImageSize;
-
-    int previewAlpha = 150, defaultAlpha = 255;
-
     double alpha = 1;
     double oneMinusAlpha;
-    double magicNumber = 92;
 
     double x0Ratio = 0.35505, y0Ratio = 0.24893;
     double ratioDistanceX = 0.07329;
     double hexagonHeightRatio = 0.08461;
-
-    int x0, y0, distance;
+    double distance;
+    int stoneImageSize;
+    int x0, y0;
 
     public GamePanel(Game game) {
         this.game = game;
@@ -56,12 +52,19 @@ public class GamePanel extends JComponent implements Observer {
         mSelected = -1;
         oneMinusAlpha = 1 - alpha;
 
-        imgPlateau=(BufferedImage) readImage("Plateau_fleches");
-        imgStonePlayer1 = (BufferedImage) readImage("Blue_Stone");
-        imgStonePlayer2 = (BufferedImage) readImage("Red_Stone");
+        loadImages();
 
         imgSrcHeight = imgPlateau.getHeight();
         imgSrcWidth = imgPlateau.getWidth();
+    }
+
+    private void loadImages() {
+        imgPlateau=(BufferedImage) readImage("Plateau_fleches");
+        imgStonePlayer1 = (BufferedImage) readImage("Blue_Stone");
+        imgStonePlayer2 = (BufferedImage) readImage("Red_Stone");
+        imgStonePlayer1Preview = (BufferedImage) readImage("Blue_Stone_transparent");
+        imgStonePlayer2Preview = (BufferedImage) readImage("Red_Stone_transparent");
+        imgStoneDisabled = (BufferedImage) readImage("Disabled_Stone");
     }
 
     @Override
@@ -84,26 +87,21 @@ public class GamePanel extends JComponent implements Observer {
             y = (int) ((oneMinusAlpha) / 2 * height);
             imageWidth = (int) Math.round(alpha * height * imgSrcWidth) / imgSrcHeight;
             imageHeight = (int) Math.round(alpha * height);
-            size = imageHeight * (magicNumber / (double)imgSrcHeight);
         }
         else{
             x = (int) (((oneMinusAlpha) / 2) * width);
             y = (int) ((height - ((alpha * width * imgSrcHeight) / imgSrcWidth)) / 2);
             imageWidth = (int) Math.round(alpha * width);
             imageHeight = (int) Math.round((alpha * width * imgSrcHeight) / imgSrcWidth);
-            size = imageHeight * (magicNumber / (double)imgSrcHeight);
         }
 
         // Calcul du centre de la case 0:0 du plateau
         x0 = (int) Math.round(x + (x0Ratio * imageWidth));
         y0 = (int) Math.round(y + (y0Ratio * imageHeight));
-        distance = (int) Math.round(ratioDistanceX * imageWidth);
+        distance = ratioDistanceX * imageWidth;
 
         // Calcule taille de l'image des pierres
         stoneImageSize = (int) Math.round(hexagonHeightRatio * imageHeight);
-
-        incX = 2 * (int) Math.round(Math.cos((Math.PI / 6)) * size); // permet d'aller au prochain coin horizontalement
-        incY = (int) Math.round(Math.sin((Math.PI / 6)) * size) + size; // permet d'aller au prochain coin verticalement
 
         g2d.drawImage(imgPlateau, x, y, imageWidth, imageHeight, null);
     }
@@ -121,18 +119,27 @@ public class GamePanel extends JComponent implements Observer {
                 if(contentType == 0 || contentType == Integer.MAX_VALUE)
                     continue;
 
-                int x = nToX(n, m) - (stoneImageSize / 2);
-                int y = mToY(m) - (stoneImageSize / 2);
+                int x = nToX(n, m) - Math.round((float) stoneImageSize / 2);
+                int y = mToY(m) - Math.round((float) stoneImageSize / 2);
 
                 switch (contentType){
                     case 1:
-                        drawStone(g2d, imgStonePlayer1, 255, x, y, stoneImageSize);
+                        drawStone(g2d, imgStonePlayer1, x, y, stoneImageSize);
                         break;
                     case 2:
-                        drawStone(g2d, imgStonePlayer2, defaultAlpha, x, y, stoneImageSize);
+                        drawStone(g2d, imgStonePlayer2, x, y, stoneImageSize);
                         break;
                     case -1:
-
+                        if(match.getCurrentPlayerIndex() == 1)
+                            continue;
+                        drawStone(g2d, imgStoneDisabled, x, y, stoneImageSize);
+                        break;
+                    case -2:
+                        if(match.getCurrentPlayerIndex() == 0)
+                            continue;
+                        drawStone(g2d, imgStoneDisabled, x, y, stoneImageSize);
+                    case -3:
+                        drawStone(g2d, imgStoneDisabled, x, y, stoneImageSize);
                 }
             }
         }
@@ -140,30 +147,39 @@ public class GamePanel extends JComponent implements Observer {
 
     /**
      * Dessine la pierre sous le curseur du joueur actif.
-     * @param g2d
+     * @param g2d Le Graphic à utiliser pour dessiner.
      */
     private void drawSelected(Graphics2D g2d){
         int m = getmSelected();
         int n = getnSelected();
         int contentType = match.getContentAt(m, n);
+        if(contentType > 0)
+            return;
+
+        int x = nToX(n, m) - (stoneImageSize / 2);
+        int y = mToY(m) - (stoneImageSize / 2);
+
         switch (contentType){
             case 0:
-                int x = nToX(n, m) - (stoneImageSize / 2);
-                int y = mToY(m) - (stoneImageSize / 2);
-                drawStone(g2d, match.getCurrentPlayerIndex() == 0? imgStonePlayer1 : imgStonePlayer2, previewAlpha, x, y, stoneImageSize);
-                break;
-            case 1:
-                break;
-            case 2:
+                drawStone(g2d, match.getCurrentPlayerIndex() == 0? imgStonePlayer1Preview : imgStonePlayer2Preview, x, y, stoneImageSize);
                 break;
             case -1:
+                if(match.getCurrentPlayerIndex() == 0)
+                    return;
+                drawStone(g2d, match.getCurrentPlayerIndex() == 0? imgStonePlayer1Preview : imgStonePlayer2Preview, x, y, stoneImageSize);
                 break;
             case -2:
+                if(match.getCurrentPlayerIndex() == 1)
+                    return;
+                drawStone(g2d, match.getCurrentPlayerIndex() == 0? imgStonePlayer1Preview : imgStonePlayer2Preview, x, y, stoneImageSize);
                 break;
-
         }
     }
 
+    /**
+     * Draw the stones eaten at the previous turn
+     * @param g2d Le Graphic à utiliser pour dessiner.
+     */
     private void drawEaten(Graphics2D g2d){
         List<Coordinate> coordinates = match.getPreviouslyEatenCrittersCoordinates();
         for (Coordinate coord : coordinates){
@@ -173,166 +189,12 @@ public class GamePanel extends JComponent implements Observer {
             int x = nToX(coord.col(), coord.line());
             int y = mToY(coord.line());
 
-            drawStone(g2d, imgEatenStone, previewAlpha, x, y, stoneImageSize);
+            drawStone(g2d, imgEatenStone, x, y, stoneImageSize);
         }
     }
 
-    private void drawStone(Graphics2D g2d, Image img, int alpha, int x, int y, int size){
-        Color previousColor = g2d.getColor();
-        g2d.setColor(new Color(previousColor.getRed(), previousColor.getGreen() ,previousColor.getBlue() , alpha));
+    private void drawStone(Graphics2D g2d, Image img, int x, int y, int size){
         g2d.drawImage(img, x, y, size, size, null);
-        g2d.setColor(previousColor);
-    }
-
-    private void drawPlateau(Graphics2D g2D) {
-
-        int width = getWidth();
-        int height = getHeight();
-
-        int n = 0;
-        int m = 0;
-        int xSelected = -1, ySelected = -1;
-        Color colorSelected = UIColor.getColor(game.getCurrentPlayerIndex() == 0 ? UIColor.BLUE: UIColor.RED);
-        int posX = (int) (width / 2.0 - 2 * incX);
-        int posXInit = posX;
-        int posY = (int) (height / 2.0 - (5 * incY - size / 2));
-
-        int nb_elem = 5;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < nb_elem; j++) {
-                if(n == nSelected && m == mSelected) {
-                    xSelected = posX;
-                    ySelected = posY;
-                }
-                switch (match.getContentAt(m, n)) {
-                    case 1:
-                        drawHexagon(g2D, posX, posY, (int) size, UIColor.getColor(UIColor.BLUE), n, m);
-                        break;
-
-                    case 2:
-                        drawHexagon(g2D, posX, posY, (int) size, UIColor.getColor(UIColor.RED), n, m);
-                        break;
-
-                    default:
-                        drawHexagon(g2D, posX, posY, (int) size, Color.WHITE, n, m);
-                        break;
-                }
-               // drawHexagon(g2D, posX, posY, (int) size, UIColor.getColor(UIColor.RED), n, m);
-                n++;
-                posX += incX;
-            }
-            m++;
-            n = 0;
-            nb_elem++;
-            posX = posXInit;
-            posX -= incX / 2;
-            posY += incY;
-            posXInit = posX;
-        }
-
-        for (int j = 0; j < nb_elem; j++) {
-            if(n == nSelected && m == mSelected) {
-                xSelected = posX;
-                ySelected = posY;
-            }
-            switch (match.getContentAt(m, n)) {
-                    case 1:
-                        drawHexagon(g2D, posX, posY, (int) size, UIColor.getColor(UIColor.BLUE), n, m);
-                        break;
-
-                    case 2:
-                        drawHexagon(g2D, posX, posY, (int) size, UIColor.getColor(UIColor.RED), n, m);
-                        break;
-
-                    default:
-                        drawHexagon(g2D, posX, posY, (int) size, Color.WHITE, n, m);
-                        break;
-            }
-            posX += incX;
-            n++;
-        }
-        n = 1;
-        m++;
-        nb_elem--;
-        posX = posXInit;
-        posX += incX / 2;
-        posY += incY;
-        posXInit = posX;
-
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < nb_elem; j++) {
-                if(n == nSelected && m == mSelected) {
-                    xSelected = posX;
-                    ySelected = posY;
-                }
-                switch (match.getContentAt(m, n)) {
-                    case 1:
-                        drawHexagon(g2D, posX, posY, (int) size, UIColor.getColor(UIColor.BLUE), n, m);
-                        break;
-
-                    case 2:
-                        drawHexagon(g2D, posX, posY, (int) size, UIColor.getColor(UIColor.RED), n, m);
-                        break;
-                    
-                    default:
-                        drawHexagon(g2D, posX, posY, (int) size, Color.WHITE, n, m);
-                        break;
-                }
-                posX += incX;
-                n++;
-            }
-            nb_elem--;
-            n = 9 - nb_elem;
-            m++;
-            posX = posXInit;
-            posX += incX / 2;
-            posY += incY;
-            posXInit = posX;
-        }
-        if(xSelected != -1) drawHexagon(g2D, xSelected, ySelected, (int) size, colorSelected, nSelected, mSelected);
-    }
-
-    /*
-     * dessine un hexagone allant du coin haut et y revenant
-     */
-    private void drawHexagon(Graphics2D g2D, int x, int y, int size, Color color, int n, int m) {
-        double angle, cos, sin, vx, rx, ry;
-        int new_x = x;
-        int new_y = y;
-        int xs[] = new int[6];
-        int ys[] = new int[6];
-        for (int i = 0; i < 6; i++) {
-            xs[i] = x;
-            ys[i] = y;
-
-            angle = ((i) * Math.PI / 3 + Math.PI / 6);// on commence en dessinant depuis le coin haut (donc angle de
-                                                      // rotation 30°)
-            cos = Math.cos(angle);
-            sin = Math.sin(angle);
-            vx = size;
-
-            // matrice de rotation
-            rx = cos * vx;
-            ry = sin * vx;
-            new_x += (int) Math.round(rx);
-            new_y += (int) Math.round(ry);
-
-            x = new_x;
-            y = new_y;
-        }
-        Color tempColor = g2D.getColor();
-        Stroke tempStroke = g2D.getStroke();
-
-        g2D.setColor(color);
-        g2D.fillPolygon(xs, ys, 6);
-
-        if(n == nSelected && m == mSelected) g2D.setColor(UIColor.getColor(UIColor.WAFFLE));
-        else g2D.setColor(Color.BLACK);
-        g2D.setStroke(new BasicStroke(size/9.0f));
-        g2D.drawPolygon(xs, ys, 6);
-
-        g2D.setColor(tempColor);
-        g2D.setStroke(tempStroke);
     }
 
     private void drawBorders(Graphics2D g2, Color c, int width, int height) {
@@ -362,8 +224,8 @@ public class GamePanel extends JComponent implements Observer {
         int n = xToN(mouseX, mouseY);
         int m = yToM(mouseY);
 
-        Configuration.info(String.format("Mouse at %d:%d", x, y));
-        if(Math.max(Math.abs(n-4), Math.abs(m-4)) <= 4 && (n != nSelected || m != mSelected)) {
+        //Configuration.info(String.format("Mouse at %d:%d", x, y));
+        if(CoordinateUtils.isInsideBoard(new Coordinate(m, n)) && (n != nSelected || m != mSelected)) {
             nSelected = n;
             mSelected = m;
             //Configuration.info("Focus sur " + n + ", " + m);
@@ -380,7 +242,7 @@ public class GamePanel extends JComponent implements Observer {
     }
 
     public int nToX(int n, int m) {
-        return (x0 + distance * (n - (m / 2)));
+        return (int) (x0 + distance * (n - ((double) m / 2)));
     }
 
     public int mToY(int m) {
