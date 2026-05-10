75af91435d41d25ed33787f989c65e0079f93012
lreneuve <zoey@amazon.com>
2026-05-10 20:06:16 +0200

    Refactorisation minimax() dans AI.java

--- src/main/java/Controller/IA/EasyAI.java
+++ src/main/java/Controller/IA/EasyAI.java
@@ -44,46 +44,5 @@ public class EasyAI extends AI{
         return best;
     }
 
-    /**
-     * Calcule la valeur d'évaluation maximale pouvant être espérée par l'IA
-     * après un nombre de coups maximal en utilisant un arbre MIN/MAX.
-     * @param match Le match sur lequel le coup doit être calculé.
-     * @param depth Le nombre de coups d'avance que l'IA doit calculer.
-     * @param playerID L'index joueur de l'IA dans le match.
-     * @param isMax Un booléen signifiant si la fonction doit maximiser (tour de l'IA) ou
-     *              minimiser (tour de l'adversaire) les résultats d'évaluation sur les
-     *              coups possibles.
-     * @return La valeur de l'évaluation la plus avantageuse pour l'IA après le nombre de coups spécifié.
-     */
-    private double minimax(Match match, int depth, int playerID, boolean isMax){
-        if (depth == 0 || match.isGameOver()){
-            return evaluate(match, playerID);
-        }
 
-        double eval;
-
-        if (isMax){
-            double maxEval = Double.MIN_VALUE;
-            List<Coordinate> possibleMoves = match.getCurrentPlayerPlayableMoves();
-            for (Coordinate move : possibleMoves){
-                Match newMatch = MatchUtils.copy(match);
-                newMatch.playMove(move.line(), move.col());
-                eval = minimax(newMatch, depth - 1, playerID, false);
-                maxEval = Math.max(eval, maxEval);
-            }
-            return maxEval;
-        }
-
-        else{
-            double minEval = Double.MAX_VALUE;
-            List<Coordinate> possibleMoves = match.getCurrentPlayerPlayableMoves();
-            for (Coordinate move : possibleMoves){
-                Match newMatch = MatchUtils.copy(match);
-                newMatch.playMove(move.line(), move.col());
-                eval = minimax(newMatch, depth - 1, (playerID+1)%2, true);
-                minEval = Math.min(eval, minEval);
-            }
-            return minEval;
-        }
-    }
 }
