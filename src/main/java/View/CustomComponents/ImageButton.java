59f66a76f64be0487c0ac2403568f011ca3da81b
loic-bouvier <loic.bouvier@protonmail.com>
2026-05-22 16:05:14 +0200

    Ajout numéro de tour au panneau d'information

--- src/main/java/View/CustomComponents/ImageButton.java
+++ src/main/java/View/CustomComponents/ImageButton.java
@@ -7,8 +7,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.io.IOException;
-import java.io.InputStream;
 
 public class  ImageButton extends JButton {
     private Image imageEnabled, imageDisabled;
